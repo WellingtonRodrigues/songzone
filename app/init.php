71d190004120d9fa45eab03b85c591804d39ee01
Wellington Rodrigues Pereira <wellington.r.varginha@gmail.com>
2017-06-04 18:34:28 -0300

    app bootstrap files rearranged

--- app/init.php
+++ app/init.php
@@ -4,3 +4,4 @@ define("BASE_URL", "http://localhost/songzone/public");
 
 require_once '../app/core/App.php';
 require_once '../app/core/Controller.php';
+require_once '../app/core/Util.php';
--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 
-define("BASE_URL", "http://localhost/songzone/public");
-
 require_once '../app/init.php';
-require_once '../app/core/Util.php';
 
 $app = new App;
